09d477f7e90f86d4cacf3b7facb8e46cd9e3f742
harrisgotgoats <56132458+harrisgotgoats@users.noreply.github.com>
2022-06-04 10:51:36 -0700

    Searcher class objects updated

--- cs172 java lucene/artifact_id3/src/test/java/group/id3/Searcher.java
+++ cs172 java lucene/artifact_id3/src/test/java/group/id3/Searcher.java
@@ -14,6 +14,7 @@ import java.nio.file.Path;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.IndexSearcher;
@@ -31,21 +32,56 @@ import org.apache.lucene.util.QueryBuilder;
 public class Searcher {
 
     //StandardQueryParser queryParser;
-    Query query;
-    IndexSearcher searcher;
-    //QueryParser queryParser;
+    
+    private IndexSearcher isearcher; //
+    private QueryBuilder builder;
+    private Directory indexDirectory;
+    private DirectoryReader ireader;
 
 
-    public Searcher(String indexDirectoryPath) throws IOException {
-        Directory indexDirectory = FSDirectory.open(new File(indexDirectoryPath).toPath());
-        //searcher = new IndexSearcher(indexDirectory);
+    public Searcher() throws IOException {
+        String userDir = System.getProperty("user.dir");
+        String indexDir = userDir + "\\Index";
+        Path path = (new File(indexDir)).toPath();
+        indexDirectory = FSDirectory.open(path);
+        ireader = DirectoryReader.open(indexDirectory);
 
+        isearcher = new IndexSearcher(ireader);
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        builder = new QueryBuilder(analyzer);
     }
+    protected void finalize() throws CorruptIndexException, IOException 
+    {
+        ireader.close();
+        indexDirectory.close();
+    }
+    public String search(String queryText) throws IOException
+     {
+         String defaultField = "content";
+         return this.search(queryText,defaultField);
+     }
+     public String search(String queryText,String field) throws IOException
+     {
+        int numResults = 10;
+        Query q = builder.createPhraseQuery(field, queryText);
+        ScoreDoc[] hits = isearcher.search(q, numResults).scoreDocs;
+        
+        String returnedStr = "";
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = isearcher.doc(hits[i].doc);
+            returnedStr += hitDoc.get("title") + "\n";
+            //returnedStr += hitDoc.get("url") + "\n";
+
+          }
+
+        return returnedStr;
+     }
 
     public static void main(String[] args) throws IOException {
         
+        /* 
         String userDir = System.getProperty("user.dir");
-        String indexDir = userDir + "\\Data";
+        String indexDir = userDir + "\\Index";
         //Searcher searcher = new Searcher(indexDir);
 
         //Define IndexSearcher
@@ -74,17 +110,18 @@ public class Searcher {
             Document hitDoc = isearcher.doc(hits[i].doc);
             System.out.println(hitDoc.get("url"));
           }
+          */
 
         //first Document content
         //Document firstDoc = isearcher.doc(hits[0].doc);
         //System.out.println("content of firstDoc"+firstDoc.get("content")+"\n");
         
-        Explanation exp = isearcher.explain(q,0);
-        System.out.println("explanation of firstDoc+n"+exp.toString());
+        //Explanation exp = isearcher.explain(q,0);
+        //System.out.println("explanation of firstDoc+n"+exp.toString());
+        Searcher searcher = new Searcher();
+        String results = searcher.search("Justin");
+        System.out.println(results);
 
-
-        ireader.close();
-        indexDirectory.close();
         //IOUtils.rm(path); //removes the entire index
 
 
