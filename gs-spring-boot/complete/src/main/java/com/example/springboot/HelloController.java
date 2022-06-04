32d3810e8a9ca711a5fe4763c922218098640719
patrickfenn <patrickdavidfenn@gmail.com>
2022-06-04 18:12:33 +0000

    added server files

---
+++ gs-spring-boot/complete/src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,57 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.QueryBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+import java.nio.file.Path;
+import java.util.Iterator; //json parsing
+
+@RestController
+public class HelloController {
+	private Indexer indexer;
+
+	@GetMapping("/")
+	public String index() throws IOException {
+		indexer = new Indexer();
+
+		return indexer.search("UFC");
+	}
+	
+	@GetMapping("/query")
+	public ResponseEntity<String> customHeader() {
+	       	return ResponseEntity.ok()
+	       		.header("Header", "Results")
+	 			.body("Insert results here");
+	}		
+
+}
